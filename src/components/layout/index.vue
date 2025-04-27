ff676508334d1d133a619acd03f1714ec5068f5a
Sam Lai <q1113111@fimai.com.tw>
2025-04-27 10:17:30 +0000

    fix: qc-4-27

--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-1 overflow-hidden">
       <SideBar />
       <div class="relative flex flex-1 overflow-hidden">
-        <div class="flex flex-grow flex-col overflow-hidden">
+        <div class="flex flex-grow flex-col overflow-hidden p-5">
           <RouterView />
         </div>
       </div>
